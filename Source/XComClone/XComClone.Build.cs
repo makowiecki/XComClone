7c0b50d3e18cb29adf54fbc54f3e6b53e42451b6
makowiecki <makowiec.review@outlook.com>
2016-07-24 19:48:33 +0200

    Added editor module to project.

--- Source/XComClone/XComClone.Build.cs
+++ Source/XComClone/XComClone.Build.cs
@@ -8,7 +8,7 @@ public class XComClone : ModuleRules
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "UnrealEd" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
