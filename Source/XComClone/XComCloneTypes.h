6107c6de41f61996965715624db390fa56e31c85
makowiecki <makowiec.review@outlook.com>
2016-08-03 20:35:06 +0200

    Added string names to enums in XComCloneTypes

--- Source/XComClone/XComCloneTypes.h
+++ Source/XComClone/XComCloneTypes.h
@@ -5,15 +5,15 @@
 UENUM()
 enum class ETileMode: uint8
 {
-	EMPTY,
-	BLOCKED,
-	ENEMY,
-	ALLY
+	EMPTY UMETA(DisplayName = "EMPTY"),
+	BLOCKED UMETA(DisplayName = "BLOCKED"),
+	ENEMY UMETA(DisplayName = "ENEMY"),
+	ALLY UMETA(DisplayName = "ALLY"),
 };
 
 UENUM()
 enum class EPlayerId: uint8
 {
-	PLAYER_1,
-	PLAYER_2
+	PLAYER_1 UMETA(DisplayName = "PLAYER 1"),
+	PLAYER_2 UMETA(DisplayName = "PLAYER 2"),
 };
