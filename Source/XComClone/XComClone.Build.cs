54c6abcade8b4c9b03a8659126f5386cccd81aea
makowiecki <makowiec.review@outlook.com>
2016-08-08 21:35:07 +0200

    Removed cable component from dependiences

--- Source/XComClone/XComClone.Build.cs
+++ Source/XComClone/XComClone.Build.cs
@@ -6,9 +6,9 @@ public class XComClone : ModuleRules
 {
 	public XComClone(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "CableComponent" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "UnrealEd", "CableComponent" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "UnrealEd" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
